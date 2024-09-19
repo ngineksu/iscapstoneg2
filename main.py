9b3e009cd9fe3433972ba3908963112a227bcdd8
ngineksu <157438124+ngineksu@users.noreply.github.com>
2024-09-19 17:48:11 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from google.cloud import pubsub_v1
+
+# TODO(developer)
+project_id = "acquired-talent-433100-q6" 
+topic_id = "projects/acquired-talent-433100-q6/topics/capstone"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+for n in range(1, 10):
+    data_str = f"Message number {n}"
+    # Data must be a bytestring
+    data = data_str.encode("utf-8")
+    # Add two attributes, origin and username, to the message
+    future = publisher.publish(
+        topic_path, data, origin="python-sample", username="gcp"
+    )
+    print(future.result())
+
+print(f"Published messages with custom attributes to {topic_path}.")
