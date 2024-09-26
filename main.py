b6bd80f36ec12e99fc5277348bc860116dba8763
ngineksu <157438124+ngineksu@users.noreply.github.com>
2024-09-26 17:28:27 -0400

    implemented button
    the button sends messages when clicked!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from google.cloud import pubsub_v1
+from tkinter import *
+from tkinter.ttk import *
 
 
 def sendMessages():
@@ -21,4 +23,48 @@ def sendMessages():
 
     print(f"Published messages with custom attributes to {topic_path}.")
 
-sendMessages()
+# below this line is copypasted from tutorial
+
+
+# creates a Tk() object
+master = Tk()
+ 
+# sets the geometry of main 
+# root window
+master.geometry("200x200")
+ 
+ 
+# function to open a new window 
+# on a button click
+def openNewWindow():
+     
+    # Toplevel object which will 
+    # be treated as a new window
+    newWindow = Toplevel(master)
+ 
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title("New Window")
+ 
+    # sets the geometry of toplevel
+    newWindow.geometry("200x200")
+
+     # A Label widget to show in toplevel
+    Label(newWindow, 
+          text ="Welcome to our project!").pack()
+ 
+ 
+label = Label(master, 
+              text ="Welcome to our project!")
+ 
+label.pack(pady = 10)
+ 
+# a button widget which will open a 
+# new window on button click
+btn = Button(master, 
+             text ="Send messages", 
+             command = sendMessages)
+btn.pack(pady = 10)
+ 
+# mainloop, runs infinitely
+mainloop()
